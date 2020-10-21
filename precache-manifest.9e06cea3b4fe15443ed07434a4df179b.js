6172371b1898d0cba76454046b7fe3eeb0b36d0b
chilot <https://github.com/Raigyo>
2020-10-21 16:03:00 +0200

    Updates

---
+++ precache-manifest.9e06cea3b4fe15443ed07434a4df179b.js
@@ -0,0 +1,42 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "5817c58d6c77cb1492f8a0686f5392b0",
+    "url": "/index.html"
+  },
+  {
+    "revision": "983f2b321844136cd2d4",
+    "url": "/static/css/2.8aa5a7f8.chunk.css"
+  },
+  {
+    "revision": "8f8fde284c023fd43320",
+    "url": "/static/css/main.3f9dc988.chunk.css"
+  },
+  {
+    "revision": "983f2b321844136cd2d4",
+    "url": "/static/js/2.7313db04.chunk.js"
+  },
+  {
+    "revision": "0933ee6533665f8da902764f3c16af6d",
+    "url": "/static/js/2.7313db04.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "8f8fde284c023fd43320",
+    "url": "/static/js/main.99ed07f5.chunk.js"
+  },
+  {
+    "revision": "aeea36bdadbfd99c7e94",
+    "url": "/static/js/runtime-main.f7969a42.js"
+  },
+  {
+    "revision": "aa811e9dc7afca8a9b193629fb9fc41d",
+    "url": "/static/media/28 Days Later.aa811e9d.woff"
+  },
+  {
+    "revision": "d201e4aa499eff8d160fe37e1f4fc941",
+    "url": "/static/media/BebasNeue.d201e4aa.woff"
+  },
+  {
+    "revision": "7ed8a39327099423f68b56e20df19d4d",
+    "url": "/static/media/WickedScaryMovie.7ed8a393.woff"
+  }
+]);
