895cae35b8a06bf64fbd736753c7e81195175048
chilot <https://github.com/Raigyo>
2020-10-21 16:43:28 +0200

    Updates

---
+++ precache-manifest.a7049f5d27889c4f18761f08e9b6bbd7.js
@@ -0,0 +1,42 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "a6e6e85f442a4d0d3d49573a4e9aa121",
+    "url": "/react-netflix-clone/index.html"
+  },
+  {
+    "revision": "5bba5e79ca6dcd07015b",
+    "url": "/react-netflix-clone/static/css/2.8aa5a7f8.chunk.css"
+  },
+  {
+    "revision": "9b52b816a781d84a8071",
+    "url": "/react-netflix-clone/static/css/main.566a1ed9.chunk.css"
+  },
+  {
+    "revision": "5bba5e79ca6dcd07015b",
+    "url": "/react-netflix-clone/static/js/2.b9ee3f67.chunk.js"
+  },
+  {
+    "revision": "0933ee6533665f8da902764f3c16af6d",
+    "url": "/react-netflix-clone/static/js/2.b9ee3f67.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "9b52b816a781d84a8071",
+    "url": "/react-netflix-clone/static/js/main.521a79df.chunk.js"
+  },
+  {
+    "revision": "081fd78762539e94860a",
+    "url": "/react-netflix-clone/static/js/runtime-main.0e8de37a.js"
+  },
+  {
+    "revision": "aa811e9dc7afca8a9b193629fb9fc41d",
+    "url": "/react-netflix-clone/static/media/28 Days Later.aa811e9d.woff"
+  },
+  {
+    "revision": "d201e4aa499eff8d160fe37e1f4fc941",
+    "url": "/react-netflix-clone/static/media/BebasNeue.d201e4aa.woff"
+  },
+  {
+    "revision": "7ed8a39327099423f68b56e20df19d4d",
+    "url": "/react-netflix-clone/static/media/WickedScaryMovie.7ed8a393.woff"
+  }
+]);
--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "/precache-manifest.df2c33efa1632d9c566ee834a298384c.js"
+  "/react-netflix-clone/precache-manifest.a7049f5d27889c4f18761f08e9b6bbd7.js"
 );
 
 self.addEventListener('message', (event) => {
@@ -33,7 +33,7 @@ workbox.core.clientsClaim();
 self.__precacheManifest = [].concat(self.__precacheManifest || []);
 workbox.precaching.precacheAndRoute(self.__precacheManifest, {});
 
-workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("/index.html"), {
+workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("/react-netflix-clone/index.html"), {
   
   blacklist: [/^\/_/,/\/[^/?]+\.[^/]+$/],
 });
